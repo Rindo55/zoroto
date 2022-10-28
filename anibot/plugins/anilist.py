175d5ae7e2d1a414cc5bcfc349d6448b865c0508
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2022-10-28 23:13:29 +0530

    Update anilist.py

--- anibot/plugins/anilist.py
+++ anibot/plugins/anilist.py
@@ -81,7 +81,7 @@ async def anime_cmd(client: Client, message: Message, mdata: dict):
     if await (SFW_GRPS.find_one({"id": gid})) and result[2].pop()=="True":
         await client.send_photo(gid, no_pic[random.randint(0, 4)], caption="This anime is marked 18+ and not allowed in this group")
         return
-    animexx = await client.send_photo(gid, title_img, caption=finals_, reply_markup=buttons)
+    animexx = await client.send_photo(gid, title_img, caption=finals_, reply_markup=buttons + InlineKeyboardMarkup([[InlineKeyboardButton("Watch Online", url=f"https://zoro.to")]]))
     if title_img not in PIC_LS:
         PIC_LS.append(title_img)
     await asyncio.sleep(180)
