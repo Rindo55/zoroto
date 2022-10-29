7c3c1263cb63a8b19695e65a3429981b4f72f8d9
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2022-10-29 19:43:23 +0530

    Update data_parser.py

--- anibot/utils/data_parser.py
+++ anibot/utils/data_parser.py
@@ -1338,8 +1338,7 @@ async def get_airing(qry, ind: int, auth: bool = False, user: int = None):
     if air_on:
         out += f"Airing Episode `{episode}{th}` in `{air_on}`"
     site = data["siteUrl"]
-    return [coverImg, out], [site, result["data"]["Page"]["pageInfo"]["total"]], [mid, in_ls, in_ls_id, isfav]
-
+    return [coverImg, out], [site, result["data"]["Page"]["pageInfo"]["total"]]
 
 async def toggle_favourites(id_: int, media: str, user: int):
     vars_ = {"id": int(id_)}
