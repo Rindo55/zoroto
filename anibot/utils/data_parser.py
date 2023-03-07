2529aac35efee61f52a3304af436c3d101707a86
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-03-07 15:04:23 +0530

    Update data_parser.py

--- anibot/utils/data_parser.py
+++ anibot/utils/data_parser.py
@@ -1009,8 +1009,7 @@ async def get_anime(vars_, auth: bool = False, user: int = None, cid: int = None
     tags = []
     for i in data['tags']:
         tags.append(i["name"])
-        tagsx = f"\n{bl}**{text[8]}:** {', #'.join(tags)}" if tags != [] else ""
-        tagsx = "#" + f"{', #'.join(tags)}"
+        tagsx = f"\n{bl}**{text[8]}:** {', #'.join(tags)}"
         tagsx = tagsx.replace("#Age Gap", "#Age_Gap")
         tagsx = tagsx.replace("#Anti-hero", "#Antihero")
         tagsx = tagsx.replace("#Artificial Intelligence", "#Artificial_Intelligence")
@@ -1198,7 +1197,6 @@ async def get_anilist(qdb, page, auth: bool = False, user: int = None, cid: int 
     for i in data['tags']:
         tags.append(i["name"])
         tagsx = f"\n{bl}**{text[8]}:** {', #'.join(tags)}" 
-        tagsx = "#" + f"{', #'.join(tags)}"
         tagsx = tagsx.replace("#Age Gap", "#Age_Gap")
         tagsx = tagsx.replace("#Anti-Hero", "#Antihero")
         tagsx = tagsx.replace("#Artificial Intelligence", "#Artificial_Intelligence")
